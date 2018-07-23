50cf1ad0b6557918449b55ade8b73cbbfecc314c
Harsha <iamharshasmart@gmail.com>
2018-07-24 02:36:47 +0530

    tests for subract and multiply and subract added

--- lib/binary_calc.rb
+++ lib/binary_calc.rb
@@ -1,5 +1,10 @@
 class Binary_calc
+  # addition
   def add(op1, op2)
     op1 + op2
+  end
+  # subraction
+  def minus(op1, op2)
+    op1 - op2
   end
 end
--- spec/binary_calc_spec.rb
+++ spec/binary_calc_spec.rb
@@ -8,5 +8,33 @@ describe Binary_calc do
         expect(bc.add(4, 2)).to eql(6)
       end
     end
+    context "adding 8 and 4" do
+      it "gives 12" do
+        bc = Binary_calc.new
+        expect(bc.add(8, 4)).to eql(12)
+      end
+    end
+  end
+  describe ".minus" do
+    context "minus 2 from 4" do
+      it "gives 2" do
+        bc = Binary_calc.new
+        expect(bc.minus(4, 2)).to eql(2)
+      end
+    end
+    context "minus 8 from 4" do
+      it "gives -4" do
+        bc = Binary_calc.new
+        expect(bc.minus(4, 8)).to eql(-4)
+      end
+    end
+  end
+  describe ".multiply" do
+    context "4 and 2 multiplies" do
+      it "gives 8" do
+        bc = Binary_calc.new
+        expect(bc.multiply(4, 2)).to eql(8)
+      end
+    end
   end
 end
